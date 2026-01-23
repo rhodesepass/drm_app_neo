6bf62adf199dc933b62fa3b352081375bbbcf45a
Shirogane Tsumugi <inapp@iccmc.cc>
2026-01-23 11:08:38 +0800

    change jpeg decoder backend to libjpeg-turbo

--- src/lv_conf.h
+++ src/lv_conf.h
@@ -946,11 +946,11 @@
 
 /** JPG + split JPG decoder library.
  *  Split JPG is a custom format optimized for embedded systems. */
-#define LV_USE_TJPGD 1
+#define LV_USE_TJPGD 0
 
 /** libjpeg-turbo decoder library.
  *  - Supports complete JPEG specifications and high-performance JPEG decoding. */
-#define LV_USE_LIBJPEG_TURBO 0
+#define LV_USE_LIBJPEG_TURBO 1
 
 /** GIF decoder library */
 #define LV_USE_GIF 1
